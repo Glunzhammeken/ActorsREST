77585982b7f547fd26ec7fd283c305f7ebda09ac
Glunzhammeken <160604689+Glunzhammeken@users.noreply.github.com>
2025-03-10 14:45:07 +0100

    Add project files.

---
+++ ActorsREST/Controllers/ActorsController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ActorReposLib;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ActorsREST.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActorsController : ControllerBase
+    {
+        private ActorReposList _actorReposList;
+        public ActorsController(ActorReposList actorReposList) 
+        {
+            _actorReposList = actorReposList;
+        }
+        // GET: api/<ActorsController>
+        [HttpGet]
+        public IEnumerable<Actor> Get()
+        {
+            return _actorReposList.GetActors();
+        }
+
+        // GET api/<ActorsController>/5
+        [HttpGet("{id}")]
+        public Actor? Get(int id)
+        {
+            return _actorReposList.GetActorById(id);
+        }
+
+        // POST api/<ActorsController>
+        [HttpPost]
+        public Actor Post([FromBody] Actor NewActor)
+        {
+            return _actorReposList.Add(NewActor);
+
+        }
+
+        // PUT api/<ActorsController>/5
+        [HttpPut("{id}")]
+        public Actor? Put(int id, [FromBody] Actor NewActor)
+        {
+            return _actorReposList.UpdateActor(id, NewActor);
+        }
+
+        // DELETE api/<ActorsController>/5
+        [HttpDelete("{id}")]
+        public Actor Delete(int id)
+        {
+            return _actorReposList.Remove(id);
+        }
+    }
+}
---
+++ ActorsREST/Program.cs
@@ -0,0 +1,19 @@
+using ActorReposLib;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddSingleton<ActorReposList>(new ActorReposList());
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
